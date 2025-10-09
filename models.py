60876321bb398b0249182f749124044a51cac5be
Talis Kalnins <taliskalnins@Nah-Fam.local>
2025-10-10 10:10:35 +1100

    frankie initial

---
+++ models.py
@@ -0,0 +1,8 @@
+import roboticstoolbox as rtb
+
+# print all urdf models
+# print(rtb.models.URDF.__all__)
+print(rtb.models.URDF.Frankie())
+print(rtb.models.URDF.FrankieOmni())
+# print(rtb.models.URDF.px100())
+# print(rtb.models.URDF.px150())
